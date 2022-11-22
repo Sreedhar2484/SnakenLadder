391fccf0e4ddac991af82c4a73efd39c46410094
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-22 12:10:17 +0530

    added RandoValue.java file

---
+++ RandomValue.java
@@ -0,0 +1,13 @@
+
+import java.util.Random;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class RandomValue{
+    public static void main(String args[]){
+        int SINGLE_PLAYER = 0;
+        int random = (int)Math.floor(Math.random()*(6-1+1)+1);
+        System.out.println(random);
+           
+    }
+}
